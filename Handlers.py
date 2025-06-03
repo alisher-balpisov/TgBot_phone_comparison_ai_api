4b25437a4f462e845284eff68cae4079387a3466
alisher <alisher200614bal@gmail.com>
2025-06-03 04:52:16 -0700

    final version v3

--- Handlers.py
+++ Handlers.py
@@ -1,9 +1,11 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
+
 from Client import client
 
 router = Router()
+
 
 @router.message(Command("start"))
 async def send_welcome(message: Message):
@@ -18,12 +20,12 @@ async def compare_phones(message: Message):
     user_input = message.text
 
     # Проверяем, содержит ли сообщение ключевые слова для сравнения
-    if " и " in user_input or " vs " in user_input.lower():
+    if " и " in user_input.lower() or " vs " in user_input.lower():
         await message.reply("Сравниваю телефоны, пожалуйста подождите...")
 
         try:
             # Запрос к OpenRouter API
-            completion = client.chat.completions.create(
+            prompt = client.chat.completions.create(
                 model="deepseek/deepseek-chat-v3-0324:free",
                 messages=[{
                     "role": "user",
@@ -38,7 +40,7 @@ async def compare_phones(message: Message):
                 Если в сравнении неизвестные тебе модели телефонов пиши что я не знаю такие модели и не могу их сравнить."""
                 }])
 
-            response = completion.choices[0].message.content
+            response = prompt.choices[0].message.content
             cleaned = (response.replace('"', '').replace("«", "").replace
                        ("»", "").replace("“", "").replace("”", "").replace(
                 "‘", "").replace("’", "").replace("###", "").
@@ -49,7 +51,7 @@ async def compare_phones(message: Message):
                 await message.answer(cleaned[i:i + 4096])
 
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error_in_Handlers: {e}")
             await message.reply("Произошла ошибка при обработке запроса. Пожалуйста, попробуйте позже.")
     else:
         await message.reply(
--- Keys.py
+++ Keys.py
@@ -1,2 +1,2 @@
 tg_token = '7444635527:AAHNhNJ_GMMcj64k0so9UOkeW4LwneoMKgI'
-api_key='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'
+api_key = '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'
